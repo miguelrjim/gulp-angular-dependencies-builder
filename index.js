9874e66603d5bf106890b6b56ce552d0546a7505
Miguel Jimenez <mjimenez@rocketlawyer.com>
2015-04-23 17:27:35 -0700

    fixing bugs

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ module.exports = function (options) {
 	}
 
 	// Loads up specified database
-	var db = new nedb({
-		filename: options.db,
-		autoload: true
-	});
+	var db = options.db;
 
 	// Pattern to match a module declaration with its dependencies
 	var moduleRegex = /\.module\(\s*(?:'|")([^'"]+)(?:'|")\s*,\s*(\[[^\]]*\])/g;
@@ -32,26 +29,35 @@ module.exports = function (options) {
 		// Get module name and its dependencies, inject them to the database
 		var contents = file.contents.toString();
 
-		function process() {
+		
+
+		function multiProcess() {
 			var results = moduleRegex.exec(contents);
-			if(results && results[1] && results[2])
+			var name = null;
+			var dependencies = null;
+			if(results && (name=results[1]) && (dependencies=results[2])) {
+				dependencies = eval(dependencies);
 				db.update(
 					{
-						name: results[1]
+						name: name
 					},
 					{
-						name: results[1],
-						dependencies: eval(results[2]),
+						name: name,
+						dependencies: dependencies,
 						path: file.path.substr(process.cwd().length+1)
+
 					},
 					{
 						upsert: true
 					}
 				, function() {
-					process();
+					multiProcess();
 				});
+			}
 			else
 				cb();
 		}
+
+		multiProcess();
 	});
 };
